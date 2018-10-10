0dc92105475ebc6df97b655bbbadcb8a22b0f256
Renan DelValle <renan@apache.org>
2018-10-10 16:51:09 -0700

    Fix style issues

--- ui/src/main/js/components/TaskListItemActions.js
+++ ui/src/main/js/components/TaskListItemActions.js
@@ -3,16 +3,15 @@ import React from 'react';
 import { isThermos } from 'utils/Task';
 
 export default function ({ task }) {
-
   if (isThermos(task.assignedTask.task)) {
     return (<div className='task-list-item-host'>
-        <a href={`http://${task.assignedTask.slaveHost}:1338/task/${task.assignedTask.taskId}`}>
-    {task.assignedTask.slaveHost}
-  </a>
+      <a href={`http://${task.assignedTask.slaveHost}:1338/task/${task.assignedTask.taskId}`}>
+        {task.assignedTask.slaveHost}
+      </a>
     </div>);
   } else {
     return (<div className='task-list-item-host'>
-    <strong>{task.assignedTask.slaveHost}</strong>
+      <strong>{task.assignedTask.slaveHost}</strong>
     </div>);
   }
 }
