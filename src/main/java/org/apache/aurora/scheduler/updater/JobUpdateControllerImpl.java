f815f7796ffbecd9e6982f9ae9d0575be2222294
Renan DelValle <renan@apache.org>
2019-03-11 18:19:54 -0700

    Add missing import for HashSet.

--- src/main/java/org/apache/aurora/scheduler/updater/JobUpdateControllerImpl.java
+++ src/main/java/org/apache/aurora/scheduler/updater/JobUpdateControllerImpl.java
@@ -16,6 +16,7 @@ package org.apache.aurora.scheduler.updater;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
